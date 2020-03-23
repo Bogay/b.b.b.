72ab6e54edf6cc39f5c501ee854e3bd1be4bc25e
Bogay <pojay11523@gmail.com>
2020-03-23 23:21:22 +0800

    Implement `BallProperty` and setup SO

--- Assets/Scripts/Ball/BallProperty.cs
+++ Assets/Scripts/Ball/BallProperty.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //讓我們可以直接在unity介面創建此class的實例
+[CreateAssetMenu()]
 public class BallProperty : ScriptableObject
 {
-    //球的傷害
-    //球的反彈
-    //球的速度
-    //砲台發射頻率
-
-    /*此行以下是各個屬性的初始數值*/
-    /*此行以上是各個屬性的初始數值*/
-
-    public void Initialize() //數據初始化
+    // Base Value
+    [SerializeField]
+    private int damage = 1;
+    [SerializeField]
+    private int reflect = 3;
+    [SerializeField]
+    private float speed = 25f;
+    [SerializeField]
+    private float firerate = 12.5f;
+    // Delta
+    private int damageDelta = 0;
+    private int reflectDelta = 0;
+    private float speedDelta = 1;
+    private float firerateDelta = 1;
+    // Exposed interface
+    public int Damage
     {
-        /*實作待補*/
+        get { return this.damage + this.damageDelta; }
     }
+    public int Reflect
+    {
+        get { return this.reflect + this.reflectDelta; }
+    }
+    public float Speed
+    {
+        get { return this.speed * this.speedDelta; }
+    }
+    public float Firerate
+    {
+        get { return this.firerate * this.firerateDelta; }
+    }
+
     public void SetValue(BallBuff buff) //更改屬性數值
     {
-        /*實作待補*/
+        this.damageDelta += buff.damage;
+        this.reflectDelta += buff.reflect;
+        this.speedDelta *= buff.speed;
+        this.firerateDelta *= buff.firerate;
     }
 }
