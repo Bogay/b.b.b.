af62a9badc11403b13f9b294b2fde5fec6750e8e
Bogay <pojay11523@gmail.com>
2020-03-23 23:17:16 +0800

    Finish ball buff definition

--- Assets/Scripts/Ball/BallBuff.cs
+++ Assets/Scripts/Ball/BallBuff.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//讓我們可以直接在unity介面創建此class的實例
+// 讓我們可以直接在unity介面創建此class的實例
+[CreateAssetMenu()]
 public class BallBuff : ScriptableObject
 {
-    //球的傷害
-    //反彈次數
-    //球的速度
-    //砲台發射頻率
+    // 球的傷害 (add)
+    public int damage = 0;
+    // 反彈次數 (add)
+    public int reflect = 0;
+    // 球的速度 (mul)
+    public float speed = 1;
+    // 砲台發射頻率 (mul)
+    public float firerate = 1;
 }
