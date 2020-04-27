a57fa4004b9740b872fe59f7b93feb780f71ca5d
Bogay <pojay11523@gmail.com>
2020-04-27 20:28:47 +0800

    Finish `Fall`

--- Assets/Scripts/Brick/Fall.cs
+++ Assets/Scripts/Brick/Fall.cs
@@ -6,7 +6,6 @@ public class Fall : MonoBehaviour
 {
     [SerializeField]
     private CountDown countDown = null; //遊戲倒數計時器
-
     /*這行以下是生成磚塊和更改磚塊用的參數*/
     [SerializeField]
     private GameObject brick = null;
@@ -27,18 +26,21 @@ public class Fall : MonoBehaviour
 
     private void Start()
     {
-        height = (int)Camera.main.orthographicSize; //取得攝像機的視野半高
+        height = (int) Camera.main.orthographicSize; //取得攝像機的視野半高
         seed = Random.Range(0.1f, 100f); //生成一個隨機亂數，讓每次生出來的磚塊有不同的排列組合
-        if (countDown != null) //如果遊戲倒數計時器不是null 
+        if(countDown != null) //如果遊戲倒數計時器不是null 
         {
-            //向OnCountDownComplete事件訂閱StartUp函式
+            //向 OnCountDownComplete 事件訂閱 StartUp 函式
+            this.countDown.OnCountDownComplete.AddListener(this.StartUp);
             //讓此物體不呼叫Update/Fixed Update...等等
-        }    
+            enabled = false;
+        }
     }
 
     private void StartUp()
     {
         //讓此物體呼叫Update/Fixed Update...等等
+        enabled = true;
     }
 
     private void FixedUpdate() //社課教學不會修改到此函式
@@ -46,25 +48,25 @@ public class Fall : MonoBehaviour
         /*這段函式負責處理帷幕的落下速度，以及每落下一單位就生一排磚塊出來*/
         transform.position += new Vector3(0, -speed * Time.fixedDeltaTime, 0);
         timer += Time.fixedDeltaTime;
-        speed = 0.25f * Mathf.Log10(Mathf.Max(timer,1)) + 0.4f;
+        speed = 0.25f * Mathf.Log10(Mathf.Max(timer, 1)) + 0.4f;
 
         addHptimer += Time.fixedDeltaTime;
-        if (addHptimer >= addHpTime * (addHp + 1))
+        if(addHptimer >= addHpTime * (addHp + 1))
             addHp++;
 
         if(transform.position.y < floor)
         {
             SpawnBrick(height - floor);
-            floor --;
+            floor--;
         }
     }
 
     private void SpawnBrick(int y) //社課教學不會修改到此函式
     {
         /*這段函式負責生磚塊的邏輯判斷，有興趣的可以去看補充ppt*/
-        for (int i = 0; i < width; i++)
+        for(int i = 0; i < width; i++)
         {
-            float value = Mathf.PerlinNoise(i/4f + seed,(y-height)/4f + seed);
+            float value = Mathf.PerlinNoise(i / 4f + seed, (y - height) / 4f + seed);
             if(value >= threshold)
             {
                 GameObject go = Instantiate(brick, new Vector3(0, 10000, 0), Quaternion.identity, transform);
